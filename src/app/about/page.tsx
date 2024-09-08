1abfc584170f5f5c065f8d0cb63af34332d65144
David Akang <akangdavd68@yahoo.com>
2024-09-08 19:37:49 +0100

    Update dependencies and add metadata to pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,17 @@
+import { Metadata } from 'next';
 import React from 'react';
 
-const AboutPage: React.FC = () => {
+export const metadata: Metadata = {
+  title: 'About Luxstay Ventures | Leading Serviced Accommodation Experts',
+  description:
+    'Learn about Luxstay Ventures, your premier choice for luxury short-stay rentals. Discover our commitment to providing top-notch accommodations that blend comfort and convenience for travelers.',
+};
+
+export default function AboutPage() {
   return (
     <div>
       <h1>About Page</h1>
       <p>Welcome to the About page!</p>
     </div>
   );
-};
-
-export default AboutPage;
+}
